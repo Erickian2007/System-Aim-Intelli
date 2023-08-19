9f6d2ca434b72b0db5f2be5ab696bc6b7edcec0a
Erickian2007 <erickian2007@gmail.com>
2023-08-19 16:30:59 -0300

    fix-head

--- Player/SpringArm.gd
+++ Player/SpringArm.gd
@@ -1,15 +1,18 @@
 # 1 - Head e Body Seguir Mouse
-# 2 - Fazer Z
+# 2 - Fazer Zoom in out
 
 extends SpringArm3D
 
 @onready var player: CharacterBody3D = $".."
-@onready var head_model: Node3D = player.get_node("Mesh/Godot_Chan_Stealth_Shooter/Godot_Chan_Stealth/Skeleton3D/Head")
+@onready var head_node: Node3D = player.get_node("Mesh/Godot_Chan_Stealth_Shooter/Godot_Chan_Stealth/Skeleton3D/Head")
 @onready var body_model: MeshInstance3D = player.get_node("Mesh/Godot_Chan_Stealth_Shooter/Godot_Chan_Stealth/Skeleton3D/armor")
 @onready var head_dir: RayCast3D = player.get_node("HeadDir")
 @onready var body_rot: Area3D = player.get_node("BodyRot")
 
 @onready var camera: Camera3D = self.get_node("Camera")
+
+@export var weight: float = 1.0
+@export var weight_return = 1.0
 
 const sensi_x = 0.002
 const sensi_y = 0.001
@@ -41,63 +44,34 @@ func _input(event: InputEvent) -> void:
 		self.rotation.y -= sensi.x
 		self.rotation.x -= sensi.y
 
-		head_model.rotation_degrees.y = wrapf(head_model.rotation_degrees.y, -360.0, 360)
+		head_node.rotation_degrees.y = wrapf(head_node.rotation_degrees.y, -360.0, 360)
 	# limit axys y of spring arm
 		self.rotation_degrees.x = (
 			clamp(self.rotation_degrees.x,
 			limit_mouse_y_min,
 			limit_mouse_y_max
 			))
-
-		# 1 
-		if (Input.is_action_pressed("aim")):
-		# head_model look mouse direction(head_dir)
-			#head_model.rotation_degrees.y = (lerp(head_model.rotation_degrees.y, head_dir.rotation_degrees.y, weight_head_look_mouse))
-			var rot_mouse = self.rotation.y + deg_to_rad(180)
-			head_model.transform.basis = Basis(Vector3(0,1,0), rot_mouse)
-			#head_model.transform.basis = Basis(Vector3(0,1,0), head_dir.transform.basis.)
-		# limit head_model deslocation y
-			head_model.rotation.x += sensi.y
-			head_model.rotation_degrees.x = (
-				clamp(head_model.rotation_degrees.x,
-					limit_head_y_min,
-					limit_head_y_max
-				))
-
-			if (head_dir.is_colliding()):
-				body_model.rotation_degrees.y = (
-					lerp(
-						body_model.rotation_degrees.y,
-						head_model.rotation_degrees.y,
-						0.3
-					))
 			
-func _process(delta: float) -> void:
-	# 1
-	if (!Input.is_action_pressed("aim")):
-	# return center head_model
-		head_model.rotation.x = (
-				lerp_angle(head_model.rotation.x,
-					0.0,
-					1 * delta
-				))
-		head_model.rotation.y = (
-				lerp_angle(head_model.rotation.y,
-					body_model.rotation.y,
-					3 * delta
-				))
-	
-	# head_dir getting rotation of spring_arm
-	head_dir.global_rotation_degrees.y = self.global_rotation_degrees.y + 180
-	
-	
-	# 2
-	# zoom in out
+func _process(_delta: float) -> void:
+	# 1 - head e body seguindo mouse
 	if (Input.is_action_pressed("aim")):
+		# head_node look mouse direction(head_dir)
+		var look_y = 0.0
+		var look_x = 0.0
+		look_y = lerp_angle(look_y, self.rotation.y + deg_to_rad(180), weight)
+		look_x = lerp_angle(look_x, -self.rotation.x, weight)
+		head_node.get_node("y/x").transform.basis = Basis(Vector3(1,0,0), look_x)
+		head_node.get_node("y").transform.basis = Basis(Vector3(0,1,0), look_y)
+		# 2 - zoom in
 		camera.keep_aspect = camera.KEEP_WIDTH
 		camera.position.x = 0.6
 	else:
+		# head_node return center
+		var center_x = lerp_angle(head_node.get_node("y/x").rotation.x, 0.0, weight_return)
+		var center_y = lerp_angle(head_node.get_node("y").rotation.y, 0.0, weight_return)
+		head_node.get_node("y/x").transform.basis = Basis(Vector3(1,0,0), center_x)
+		head_node.get_node("y").transform.basis = Basis(Vector3(0,1,0), center_y)
+		# 2 - zoom out
 		camera.keep_aspect = camera.KEEP_HEIGHT
 		camera.position.x = 0.0	
-
 	body_rot.rotation.y = body_model.rotation.y
